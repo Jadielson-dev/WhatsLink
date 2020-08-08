bab2c6bd0dc754d936371b92c8bc3e729744ea6c
Jadielson Santos <69129052+Jadielson-dev@users.noreply.github.com>
2020-08-07 21:16:02 -0300

    atualizando projeto

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-console.log("Projetos em desenvolvimentos..")
+var mensagens = document.getElementById("mensagens");
+var numero = document.getElementById("numero");
+function gera() {
+	var url = document.getElementById("url");
+	var pronto = mensagens.value;
+	var api =  `https://api.whatsapp.com/send?phone=55${numero.value}&text=${pronto.replace(' ', '%20')}`;
+	url.innerHTML = api;
+}
